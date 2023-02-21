a56ec925244f6fac54a4b454705fb0a128add022
alex-255 <okozlov7@gmail.com>
2023-02-21 16:22:42 -0700

    added site logo to Theme Customizer

--- functions.php
+++ functions.php
@@ -1,8 +1,46 @@
 <?php
 
-function bd_business_files() {
-  wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap');
+function wb_business_files() {
+  wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css2?family=Libre+Franklin:wght@500&display=swap');
+  wp_enqueue_style('bootstrap-style', '//cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css');
+  wp_enqueue_style('main-style', get_stylesheet_uri());
 }
 
-// bd - means Brothers Design
-add_action('wp_enqueue_scripts', 'bd_business_files');
+// Wooden Business Customizer
+add_action('wp_enqueue_scripts', 'wb_business_files');
+
+function wb_customize_register( $wp_customize ) {
+    $wp_customize->add_section( 'wb_theme_options', array(
+        'title' => __( 'WB Theme Options' ),
+        'description' => __( 'Add Theme Options.' ),
+        'panel' => '', // Not typically needed.
+        'priority' => 300,
+        'capability' => 'edit_theme_options',
+        'theme_supports' => '', // Rarely needed.
+      ) );
+
+
+      $wp_customize->add_setting( 'wb_site_logo', array(
+        'type' => 'theme_mod', // or 'option'
+        'capability' => 'edit_theme_options',
+        'theme_supports' => '', // Rarely needed.
+        'default' => get_theme_file_uri('/images/site-logo.png'),
+        'transport' => 'refresh', // or postMessage
+        'sanitize_callback' => 'esc_url_raw',
+        'sanitize_js_callback' => '', // Basically to_json.
+      ) );
+
+      $wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'wb_site_logo_control', array(
+        'label' => 'Upload Logo',
+        'priority' => 20,
+        'section' => 'wb_theme_options',
+        'settings' => 'wb_site_logo',
+        'button_labels' => array(// All These labels are optional
+                    'select' => 'Select Site Logo',
+                    'remove' => 'Remove Site Logo',
+                    'change' => 'Change Site Logo',
+                    )
+    )) );
+
+  }
+  add_action( 'customize_register', 'wb_customize_register' );
--- header.php
+++ header.php
@@ -5,3 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
   </head>
+  <body>
+    <header>
+        <div class="menu-bar container-xxl">
+            <div class="menu-bar-hamburger"></div>
+            <div class="row">
+                <div class="col-12 col-lg-3 logo-area">
+                    <img src="<?php echo get_theme_mod( 'wb_site_logo' ); ?>">
+                </div>
+                <div class="col-12 col-lg-9 menu-area">
+                <a href="#" class="menu-area-link">Home</a>
+                <a href="#section-team" class="menu-area-link">Product</a>
+                <a href="#section-sales" class="menu-area-link">Pricing</a>
+                <a href="#section-subscribe" class="menu-area-link">Contact</a>
+                </div>
+            </div>
+        </div>
+
+    </header>
