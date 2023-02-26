648eac1dd124f680e2f4f2bc64c78d95671db217
alex-255 <okozlov7@gmail.com>
2023-02-26 14:19:58 -0700

    added index.php template

--- front-page.php
+++ front-page.php
@@ -1,10 +1,11 @@
 <?php
 get_header(); 
 ?>
+<!-- Hero images carousel -->
 <div class="container-xxl px-0">
     <div class="row">
         <div class="col-12">
-            <div id="carouselExampleCaptions" class="carousel slide">
+            <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-indicators">
                     <?php
                         // Arguments
@@ -35,7 +36,8 @@ get_header();
                         // Arguments
                         $args = array(
                             'posts_per_page' => -1,
-                            'post_type' => 'carousel'
+                            'post_type' => 'carousel',
+                            'order' => "ASC"
                         );
 
                         // The Query
@@ -53,9 +55,6 @@ get_header();
                                         <?php echo wp_trim_words(get_the_content(), 20); ?>
                                         </div>
                                     </div>
-
-
-                                    
                                 <?php
                             }
 
@@ -72,12 +71,42 @@ get_header();
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
                     <span class="visually-hidden">Next</span>
                 </button>
-                </div>
-
-
             </div>
+        </div>
     </div>
-    
+</div>
+<div class="container-xxl main-post">
+    <?php
+    // Arguments
+    $args = array(
+        'posts_per_page' => 10,
+        'post_type' => 'post'
+    );
+
+    // The Query
+    $the_query = new WP_Query( $args );
+
+    // The Loop
+    if ( $the_query->have_posts() ) {
+
+        while ( $the_query->have_posts() ) {
+            $the_query->the_post(); ?>
+                <div class="row">
+                    <div class="col-12 col-lg-7 main-post--image">
+                        <img  src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'large'); ?>" />
+                    </div>
+                    <div class="col-12 col-lg-5 main-post--content">
+                        <h2><?php the_title(); ?></h2>
+                        <p><?php echo wp_trim_words(get_the_content(), 50); ?> <a href="<?php the_permalink(); ?>">Read more &raquo;</a></p>
+                    </div>
+                    <hr>
+                </div>
+            <?php
+        }
+    } 
+    /* Restore original Post Data */
+    wp_reset_postdata(); ?>
+
 </div>
 
 <?php
