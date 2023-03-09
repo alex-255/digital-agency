9b4950d7b11f513ae5622045f4735e1cda3c148e
alex-255 <okozlov7@gmail.com>
2023-03-09 15:44:40 -0700

    style of single.php

--- single.php
+++ single.php
@@ -3,29 +3,34 @@ get_header();
 ?>
 
 <div class="container-xxl main-single-post">
-    <?php
-        if ( have_posts() ) :
-            while ( have_posts() ) : the_post(); ?>
-                <div class="row">
-                    <div class="col-12 main-single-post--image">
-                        <img  src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'large'); ?>" />
-                    </div>
-                    <div class="col-12 main-single-post--content">
-                        <h2><?php the_title(); ?></h2>
-                        <?php the_content(); ?>
-                        <?php edit_post_link(); ?>
-                        
-                    </div>
-                    <hr>
-                </div>
-            <?php 
-            endwhile;
-        else :
-            _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
-        endif;
-    ?>
     <div class="row">
-        <div class="col-12">
+        <div class="col-12 col-lg-3">
+            <?php if ( is_active_sidebar( 'primary' ) ) { ?>
+                <?php dynamic_sidebar('primary'); ?>
+            <?php } ?>
+        </div>
+        <div class="col-12 col-lg-9">
+            <?php
+                if ( have_posts() ) :
+                    while ( have_posts() ) : the_post(); ?>
+                        <div class="row">
+                            <div class="col-12 main-single-post--image">
+                                <img  src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'large'); ?>" />
+                            </div>
+                            <div class="col-12 main-single-post--content">
+                                <h2><?php the_title(); ?></h2>
+                                <?php the_content(); ?>
+                                <?php edit_post_link(); ?>
+                                
+                            </div>
+                            <hr>
+                        </div>
+                    <?php 
+                    endwhile;
+                else :
+                    _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
+                endif;
+            ?>
             <div class="comments">
             <?php 
                 // If comments are open or we have at least one comment, load up the comment template.
